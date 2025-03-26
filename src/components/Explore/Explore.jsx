55656773754d2536f0e4065c7aac2bde4b341c4a
Prossytech001 <prosperonyebuchi01@gmail.com>
2025-03-26 07:27:04 -0700

    third

--- src/components/Explore/Explore.jsx
+++ src/components/Explore/Explore.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../Explore/Explore.css'
+import { Link } from 'react-router-dom'
 
 const Explore = () => {
   return (
     <div className='explore'>
       <h1>Explore With Us In Find Your Desire Product</h1>
-      <button>Shop Now</button>
+      <Link to="/shop"><button>Shop Now</button></Link>
     </div>
   )
 }
